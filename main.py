eb6d4118ae9420bf138ae15e2d89226697a6eaa2
YunaH <Yunahan53@gmail.com>
2026-05-29 13:48:47 -0400

    Got the bot to click on cookie and check upgrades, initial code.

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from datetime import datetime, timedelta
+import schedule
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# Create and configure the chrome driver
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to the Cookie Clicker page & maximize window
+driver.get("https://ozh.github.io/cookieclicker/")
+driver.set_window_position(2568, 722)
+driver.maximize_window()
+time.sleep(1)
+
+# Find and select English language prompted
+language_selection = driver.find_element(By.CSS_SELECTOR, "#langSelect-EN")
+language_selection.click()
+time.sleep(1)
+
+# Set the end time
+end = datetime.now() + timedelta(hours=1)
+end_time = end.strftime("%Y-%m-%d %H:%M:%S")
+
+# Find the big cookie
+big_cookie = driver.find_element(By.CSS_SELECTOR, "#bigCookie")
+
+
+# Functions
+def click_big_cookie():
+    big_cookie.click()
+    time.sleep(0.05)
+
+def check_upgrades():
+    print("Checking upgrades...")
+    available_upgrades = driver.find_elements(By.CSS_SELECTOR, ".product.unlocked.enabled")
+    item_ids = [item.get_attribute("id") for item in available_upgrades]
+    print(item_ids)
+
+    for item_id in item_ids:
+        try:
+            driver.find_element(By.ID, item_id).click()
+            print(f"{item_id} upgraded")
+            time.sleep(1)
+        except Exception as e:
+            print(f"Could not find {item_id}. {e}")
+
+
+# Schedule a check every minute
+schedule.every(5).minutes.do(check_upgrades)
+
+# Keep it running until end
+while datetime.now() < end:
+    schedule.run_pending()
+    click_big_cookie()
+
+print("Done - 60 minutes passed.")
